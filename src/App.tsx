40f732e5fab31bc42bb9fa77b75a88e829b5e41a
AlexK150 <aleksandrstp1@gmail.com>
2020-10-22 16:06:04 +0300

    improved with props and PropType

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,35 @@
 import React from 'react';
+import {TodoList} from './TodoList';
 import './App.css';
 
-function App() {
-    return (
-        <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
-        </div>
-    );
+export type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
 }
 
-export default App;
+const tasks1: Array<TaskType> = [
+    {id: 1, title: 'Apples', isDone: false},
+    {id: 2, title: 'Pineapples', isDone: false},
+    {id: 3, title: 'Carrots', isDone: true},
+]
+
+const tasks2: Array<TaskType> = [
+    {id: 1, title: 'TS', isDone: false},
+    {id: 2, title: 'HTML', isDone: true},
+    {id: 3, title: 'React', isDone: true},
+]
+
+const tasks3: Array<TaskType> = [
+    {id: 1, title: 'Cinema', isDone: false},
+    {id: 2, title: 'Square', isDone: true},
+    {id: 3, title: 'Street', isDone: true},
+]
+
+export const App = () => (
+    <div className="App">
+        <TodoList tasks={tasks1} title={'What to buy'}/>
+        <TodoList tasks={tasks2} title={'What to learn'}/>
+        <TodoList tasks={tasks3} title={'Where to go'}/>
+    </div>
+);
